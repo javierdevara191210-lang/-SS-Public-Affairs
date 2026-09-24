aa17feec168d3f98010cc540dbf0a7350e4d5c04
javierdevara191210-lang <javierdevara191210@gmail.com>
2026-09-24 20:27:48 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,8 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
-        type: 4,               // Custom
+        state: "Looking over the Secret Service Department",     // this is what people actually see
+        type: 3,               // Custom
       },
     ],
   },
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#303030",
+      secondary: "#303030",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
